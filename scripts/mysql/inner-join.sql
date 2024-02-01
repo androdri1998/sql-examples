d4b2951cfe300a148df7e0250acd8db26a4b8c93
André Rodrigues <andre.rodrigues98@outlook.com>
2024-02-01 19:17:14 -0300

    feat: optimizing selects

--- scripts/mysql/inner-join.sql
+++ scripts/mysql/inner-join.sql
@@ -1,11 +1,14 @@
 SELECT 
-  p.*, 
+  p.id,
+  p.name,
+  p.description, 
   s.amount 
 FROM products p
 INNER JOIN stock s ON s.productId = p.id;
 
 -- OUTPUT
--- "id","name","description","createdAt","updatedAt","amount"
--- 1,object 1,simple description of object 1,"2024-01-31 21:27:50","2024-01-31 21:27:50",10
--- 2,object 2,simple description of object 2,"2024-01-31 21:27:55","2024-01-31 21:27:55",20
--- 4,object 4,simple description of object 4,"2024-01-31 21:28:10","2024-01-31 21:28:10",40
+-- "id","name","description","amount"
+-- 1,new object 1,simple description of object 1,10
+-- 2,object 2,simple description of object 2,20
+-- 4,object 4,simple description of object 4,40
+
--- scripts/mysql/right-join.sql
+++ scripts/mysql/right-join.sql
@@ -7,8 +7,7 @@ RIGHT JOIN products p ON p.id = s.productId;
 
 -- OUTPUT
 -- "name","amount","productId"
--- object 1,10,1
+-- new object 1,10,1
 -- object 2,20,2
 -- object 4,40,4
 -- object 5,,
--- object 6,,
