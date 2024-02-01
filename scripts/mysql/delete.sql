198a7d65567cb2bb410b5c9b45026716fa332a48
André Rodrigues <andre.rodrigues98@outlook.com>
2024-02-01 18:49:53 -0300

    feat: optimizing selects

--- scripts/mysql/delete.sql
+++ scripts/mysql/delete.sql
@@ -1,11 +1,15 @@
 DELETE FROM products
-WHERE id=9;
+WHERE id=10;
 
-SELECT * FROM products p;
+SELECT 
+  p.id, 
+  p.name, 
+  p.description 
+FROM products p;
 
 -- OUTPUT
--- "id","name","description","createdAt","updatedAt"
--- 1,new object 1,simple description of object 1,"2024-01-31 21:27:50","2024-01-31 21:27:50"
--- 2,object 2,simple description of object 2,"2024-01-31 21:27:55","2024-01-31 21:27:55"
--- 4,object 4,simple description of object 4,"2024-01-31 21:28:10","2024-01-31 21:28:10"
--- 5,object 5,simple description of object 5,"2024-01-31 21:28:16","2024-01-31 21:28:16"
+-- "id","name","description"
+-- 1,new object 1,simple description of object 1
+-- 2,object 2,simple description of object 2
+-- 4,object 4,simple description of object 4
+-- 5,object 5,simple description of object 5
--- scripts/mysql/update.sql
+++ scripts/mysql/update.sql
@@ -2,8 +2,12 @@ UPDATE products
 SET name="new object 1" 
 WHERE id=1;
 
-SELECT * FROM products p WHERE id=1;
+SELECT 
+  p.id, 
+  p.name, 
+  p.description 
+FROM products p WHERE id=1;
 
 -- OUTPUT
--- "id","name","description","createdAt","updatedAt"
--- 1,new object 1,simple description of object 1,"2024-01-31 21:27:50","2024-01-31 21:27:50"
+-- "id","name","description"
+-- 1,new object 1,simple description of object 1
