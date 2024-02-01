0b95a2cee3da50328067d34f22bc008899b1a604
André Rodrigues <andre.rodrigues98@outlook.com>
2024-02-01 19:47:14 -0300

    feat: optimizing comments

--- scripts/mysql/transactions.sql
+++ scripts/mysql/transactions.sql
@@ -10,27 +10,35 @@ VALUES(
 
 UPDATE products SET name='object 1' WHERE id=1;
 
-SELECT * FROM products p;
+SELECT 
+	p.id,
+	p.name,
+	p.description
+FROM products p;
 
 -- OUTPUT
--- "id","name","description","createdAt","updatedAt"
--- 1,object 1,simple description of object 1,"2024-01-31 21:27:50","2024-01-31 21:27:50"
--- 2,object 2,simple description of object 2,"2024-01-31 21:27:55","2024-01-31 21:27:55"
--- 4,object 4,simple description of object 4,"2024-01-31 21:28:10","2024-01-31 21:28:10"
--- 5,object 5,simple description of object 5,"2024-01-31 21:28:16","2024-01-31 21:28:16"
--- 10,object 6,simple description to object 6,"2024-02-01 20:29:11","2024-02-01 20:29:11"
+-- "id","name","description"
+-- 1,object 1,simple description of object 1
+-- 2,object 2,simple description of object 2
+-- 4,object 4,simple description of object 4
+-- 5,object 5,simple description of object 5
+-- 12,object 6,simple description to object 6
 
 COMMIT;
 
-SELECT * FROM products p;
+SELECT 
+	p.id,
+	p.name,
+	p.description
+FROM products p;
 
 -- OUTPUT
--- "id","name","description","createdAt","updatedAt"
--- 1,object 1,simple description of object 1,"2024-01-31 21:27:50","2024-01-31 21:27:50"
--- 2,object 2,simple description of object 2,"2024-01-31 21:27:55","2024-01-31 21:27:55"
--- 4,object 4,simple description of object 4,"2024-01-31 21:28:10","2024-01-31 21:28:10"
--- 5,object 5,simple description of object 5,"2024-01-31 21:28:16","2024-01-31 21:28:16"
--- 10,object 6,simple description to object 6,"2024-02-01 20:29:11","2024-02-01 20:29:11"
+-- "id","name","description"
+-- 1,object 1,simple description of object 1
+-- 2,object 2,simple description of object 2
+-- 4,object 4,simple description of object 4
+-- 5,object 5,simple description of object 5
+-- 12,object 6,simple description to object 6
 
 -- ROLLBACK EXAMPLE
 START TRANSACTION;
@@ -47,22 +55,24 @@ UPDATE products SET name='object 1' WHERE id=1;
 SELECT * FROM products p;
 
 -- OUTPUT
--- "id","name","description","createdAt","updatedAt"
--- 1,object 1,simple description of object 1,"2024-01-31 21:27:50","2024-01-31 21:27:50"
--- 2,object 2,simple description of object 2,"2024-01-31 21:27:55","2024-01-31 21:27:55"
--- 4,object 4,simple description of object 4,"2024-01-31 21:28:10","2024-01-31 21:28:10"
--- 5,object 5,simple description of object 5,"2024-01-31 21:28:16","2024-01-31 21:28:16"
--- 10,object 6,simple description to object 6,"2024-02-01 20:29:11","2024-02-01 20:29:11"
--- 11,object 7,simple description to object 7,"2024-02-01 20:31:34","2024-02-01 20:31:34"
+-- "id","name","description"
+-- 1,object 1,simple description of object 1
+-- 2,object 2,simple description of object 2
+-- 4,object 4,simple description of object 4
+-- 5,object 5,simple description of object 5
+-- 12,object 6,simple description to object 6
+-- 13,object 7,simple description to object 7
+
 
 ROLLBACK;
 
 SELECT * FROM products p;
 
 -- OUTPUT
--- "id","name","description","createdAt","updatedAt"
--- 1,object 1,simple description of object 1,"2024-01-31 21:27:50","2024-01-31 21:27:50"
--- 2,object 2,simple description of object 2,"2024-01-31 21:27:55","2024-01-31 21:27:55"
--- 4,object 4,simple description of object 4,"2024-01-31 21:28:10","2024-01-31 21:28:10"
--- 5,object 5,simple description of object 5,"2024-01-31 21:28:16","2024-01-31 21:28:16"
--- 10,object 6,simple description to object 6,"2024-02-01 20:29:11","2024-02-01 20:29:11"
+-- "id","name","description"
+-- 1,object 1,simple description of object 1
+-- 2,object 2,simple description of object 2
+-- 4,object 4,simple description of object 4
+-- 5,object 5,simple description of object 5
+-- 12,object 6,simple description to object 6
+
